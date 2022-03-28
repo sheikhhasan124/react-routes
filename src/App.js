630f9afc0a0f8f4a745fd59526683c8df6fd00f0
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-28 21:35:56 +0600

    routes work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Route, Routes,  } from 'react-router-dom';
 import Friend from './component/friend/Friend';
 import Home from './component/home/Home';
+import NotFound from './component/notFound/NotFound';
 import Service from './component/servise/Service';
 
 const App = () => {
     return (
         <div>
-            <Home />
-            <Friend />
-            <Service />
+            <h2>well come to route project</h2>
+          
+               <Routes>
+                        <Route path="/" element={ <Home />}></Route>
+                        <Route path="/friend" element={<Friend /> }></Route>
+                        <Route path="/service" element={ <Service />}></Route>
+                        <Route path="*" element={<NotFound />} ></Route>
+               </Routes>
+            
+            
         </div>
     );
 };
